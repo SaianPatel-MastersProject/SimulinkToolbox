a87f20e2f13f3f56a6a98b53072f8af7b63322e9
Saian Patel <saianp@icloud.com>
2025-02-09 09:51:01 +0000

    Configure for SM41

--- SetRef.m
+++ SetRef.m
@@ -1,7 +1,4 @@
 %% Set xRef and yRef
-% AIW_Table = readtable('+PostProcessing\+CTE\Arrow_IP.csv');
-% AIW_Table = readtable('+PostProcessing\+CTE\2kF_Circ_100_CCW.csv');
-% AIW_Table = readtable('+PostProcessing\+CTE\2kF_SUZE9.csv');
 AIW_Table = Utilities.fnLoadAIW('SUZ');
 xRefOriginal = [AIW_Table.x];
 yRefOriginal = [AIW_Table.y];
@@ -13,7 +10,7 @@ xRef = Utilities.fnInterpolateByDist([xRefOriginal, yRefOriginal], xRefOriginal,
 yRef = Utilities.fnInterpolateByDist([xRefOriginal, yRefOriginal], yRefOriginal, 0.1, 'spline');
 kappaInterp = Utilities.fnInterpolateByDist([xRefOriginal, yRefOriginal], kappa, 0.1, 'spline');
 
-% %% Test interpolation
+%% Test interpolation
 % dBetweenPoints = (sqrt(diff(xRef).^2 + diff(yRef).^2));
 % rollingDistance = [0; cumsum(dBetweenPoints)];
 % dNew = (linspace(0, rollingDistance(end), 10000))';
